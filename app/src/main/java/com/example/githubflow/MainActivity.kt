a9828f4e0d8508a6b22c306c05b55472dbd7c072
David Svensson <davidsvson@gmail.com>
2022-10-06 13:22:36 +0200

    added comment

--- app/src/main/java/com/example/githubflow/MainActivity.kt
+++ app/src/main/java/com/example/githubflow/MainActivity.kt
@@ -14,3 +14,5 @@ class MainActivity : AppCompatActivity() {
 //1. en person skapar projektet
 //2. samma person gör en git init
 //3. samma person skapar ett projekt på github och pushar upp projektet
+
+//4. alla andra person gör en clone
