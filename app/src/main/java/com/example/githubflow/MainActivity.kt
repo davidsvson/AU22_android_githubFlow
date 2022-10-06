00d2db359882023903019926d82df8202b447a04
Emil Åkerman <emil@Emils-MacBook-Air.local>
2022-10-06 13:49:23 +0200

    added new thing

--- app/src/main/java/com/example/githubflow/MainActivity.kt
+++ app/src/main/java/com/example/githubflow/MainActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var emil : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(emil)
+
     }
 }
 
@@ -14,3 +20,9 @@ class MainActivity : AppCompatActivity() {
 //1. en person skapar projektet
 //2. samma person gör en git init
 //3. samma person skapar ett projekt på github och pushar upp projektet
+
+
+
+// EMIL TESTAR GREJER
+
+
