cd268fc264facd74619895c45673dc903571d26c
Emil Åkerman <emil@Emils-MacBook-Air.local>
2022-10-06 13:52:35 +0200

    comments

--- app/src/main/java/com/example/githubflow/MainActivity.kt
+++ app/src/main/java/com/example/githubflow/MainActivity.kt
@@ -21,6 +21,16 @@ class MainActivity : AppCompatActivity() {
 //2. samma person gör en git init
 //3. samma person skapar ett projekt på github och pushar upp projektet
 
+//ALLA andra gör en clone
+//alal skapr sin egen branch git checkout -b branchname
+//gör förändringar
+
+//pusha upp din gren till github ( git push origin branchname)
+//på github gör en pull request
+//någon annan ( ej jag) mergar min pullrequest på github
+
+//jag byter gren till main
+
 
 
 // EMIL TESTAR GREJER
