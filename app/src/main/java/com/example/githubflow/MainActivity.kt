75ff8c6f65bd7a2c6c2002eaf14684730ecf8373
David Svensson <davidsvson@gmail.com>
2022-10-06 13:15:15 +0200

    first commit

---
+++ app/src/main/java/com/example/githubflow/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.githubflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+//1. en person skapar projektet
+//2. samma person gör en git init
+//3. samma person skapar ett projekt på github och pushar upp projektet
